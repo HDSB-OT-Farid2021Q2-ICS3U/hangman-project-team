489b369fed21043179c1d9aeeee2b56bafe12da2
ZCarat <74550632+Zcarat@users.noreply.github.com>
2021-01-15 13:38:04 -0500

    made guessing function

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ word = "apple"
 #TODO check w/others
 
 def blanks(word):
-    '''checks if letter inputed is in the word'''
-    letter = input("Enter a letter: ")
+    '''prints the blanks of the word'''
     blanks = [x if x == letter.lower() else "_" for x in word] 
     print(" ".join(blanks))
 
 blanks(word)
+
+def guessing(word):
+    '''user is going to guess the word, checks if it's right'''
+    guess = input("Enter your guess: ")
+    if guess.lower() == word:
+        print("Yay you guessed it!")
+    else:
+        print("Oops, try again")
+
+#TODO make the hangman so it'll draw another part if wrong
 
 #wow its beautiful
