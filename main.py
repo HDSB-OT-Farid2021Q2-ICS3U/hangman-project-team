b034c6d6b4634c3f14b162e302260b986a996b33
nicole16 <1pintonic@hdsb.ca>
2021-01-15 18:26:41 +0000

    trial

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# do stuff
+# 
+# TODO: enter question details here
+# 
+# date and time completed: yyyy-mm-dd hh:mm
+
+import word_lists
+
+print(word_lists.x)
---
+++ word_lists.py
@@ -0,0 +1,1 @@
+x = ['chicken', 'egg']
