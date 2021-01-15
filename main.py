257b906f9bf039c818f8308f4a440c1450aee807
ZCarat <74550632+Zcarat@users.noreply.github.com>
2021-01-15 14:03:38 -0500

    changed a few things

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-word = "apple"
-
 #TODO check w/others
 
 def blanks(word):
     '''prints the blanks of the word'''
     blanks = [x if x == letter.lower() else "_" for x in word] 
     print(" ".join(blanks))
-
-blanks(word)
 
 def guessing(word):
     '''user is going to guess the word, checks if it's right'''
@@ -18,5 +14,3 @@ def guessing(word):
         print("Oops, try again")
 
 #TODO make the hangman so it'll draw another part if wrong
-
-#wow its beautiful
