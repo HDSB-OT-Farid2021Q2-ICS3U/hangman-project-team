8404789d21ef1b7f8326e682c0b66e65fd9662e8
ZCarat <74550632+Zcarat@users.noreply.github.com>
2021-01-15 13:10:10 -0500

    wrote the function for blanks

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# lol hi
+word = "apple"
+
+#TODO check w/others
+
+def blanks(word):
+    '''checks if letter inputed is in the word'''
+    letter = input("Enter a letter: ")
+    blanks = [x if x == letter.lower() else "_" for x in word] 
+    print(" ".join(blanks))
+
+blanks(word)
+
+#wow its beautiful
