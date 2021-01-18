7018be2a4460fbcbc2d2dd2a6a63f275e7b99e69
ZCarat <74550632+Zcarat@users.noreply.github.com>
2021-01-18 10:15:06 -0500

    finished???

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#asking so we know which clear to use
 import os 
 system = input("Do you use Windows or Mac? ")
 if system.lower() == "windows":
@@ -5,7 +6,7 @@ if system.lower() == "windows":
 elif system.lower() == "mac":
     clear = lambda: os.system('clear')
 else:
-    cls = lambda: os.system('cls)   
+    cls = lambda: os.system('cls')   
 
 #computer picks word (from word list) that is going to be guessed
 import random
@@ -15,25 +16,18 @@ x = random.randint(0, len(wordlist.wordlist)-1) # position of random word in wor
 
 word = wordlist.wordlist.pop(x) # random word
 
+# function that prints the "_"
 def blanks(word, letter, blank):
     where = word.find(letter)
     blank[where] = letter
     print(" ".join(blank))
-
-def hangman():
-  letter = input('Enter a letter guess:')
-  wrongguesses=0
-  if letter not in word:
-    wrongguesses+=1
-  while wrongguesses<10:
-    letter=input('Enter a letter guess:')
     
 def game(word):
     print("~Hangman!~")
     blank = [x if x == " " else "_" for x in word]
     print(" ".join(blank))
-    guess = ""
-    while guess != word:
+    wrongguesses = 0
+    while wrongguesses < 10:
         which = input("Would you like to guess a letter or the word? ")
         if which.lower() == "letter":
             letter = input("Enter a letter: ")
@@ -41,6 +35,7 @@ def game(word):
                 blanks(word, letter, blank)
             else:
                 print(" ".join(blank))
+                wrongguesses += 1
         elif which.lower() == "word":
             guess = input("Enter your guess: ")
             if guess.lower() == word:
@@ -52,11 +47,13 @@ def game(word):
 game(word)
     
 #Final Question to continue the game
-while True: 
-  done = input('Are you done playing the game?(yes or no)')
-  if done == 'yes':
+done = input('Are you done playing the game?(yes or no)')
+if done == 'yes':
     print('Thank you for playing!')
-    clear
-  elif done == "no":
+    if system == "mac":
+        clear()
+    else:
+        cls()
+elif done == "no":
     game()
 
