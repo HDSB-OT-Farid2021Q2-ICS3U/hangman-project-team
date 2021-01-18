1f0300ba93124dcaba8b4f26a15c3e75e56f7b7d
ZCarat <74550632+Zcarat@users.noreply.github.com>
2021-01-18 09:54:37 -0500

    finished game function

--- main.py
+++ main.py
@@ -5,26 +5,34 @@ import wordlist # list of possible words
 
 x = random.randint(0, len(wordlist.wordlist)-1) # position of random word in word list
 
-word=wordlist.wordlist.pop(x) # random word
+word = wordlist.wordlist.pop(x) # random word
 
+def blanks(word, letter, blank):
+    where = word.find(letter)
+    blank[where] = letter
+    print(" ".join(blank))
 
+def game(word):
+    print("~Hangman!~")
+    blank = [x if x == " " else "_" for x in word]
+    print(" ".join(blank))
+    guess = ""
+    while guess != word:
+        which = input("Would you like to guess a letter or the word? ")
+        if which.lower() == "letter":
+            letter = input("Enter a letter: ")
+            if letter in word:
+                blanks(word, letter, blank)
+            else:
+                print(" ".join(blank))
+        elif which.lower() == "word":
+            guess = input("Enter your guess: ")
+            if guess.lower() == word:
+                print("Yay you guessed it!")
+                break
+            else:
+                print("Oops, try again")
 
-#TODO check w/others
+game(word)
 
-letter=input('Enter a letter guess:')
-
-def blanks(word):
-    '''prints the blanks of the word'''
-    blanks = [x if x == letter.lower() else "_" for x in word] 
-    print(" ".join(blanks))
-
-def guessing(word):
-    '''user is going to guess the word, checks if it's right'''
-    guess = input("Enter your guess: ")
-    if guess.lower() == word:
-        print("Yay you guessed it!")
-    else:
-        print("Oops, try again")
-
-#TODO make the hangman so it'll draw another part if wrong
 
